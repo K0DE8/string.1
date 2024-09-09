25e27039d52b7e0eb142559af9bf00e919fee064
Yug Rohra <149613256+K0DE8@users.noreply.github.com>
2024-09-10 01:26:58 +0530

    Add files via upload

---
+++ string_length.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () {
+    string a;
+    cin >> a;
+    int b;
+    b=a.size();
+    cout << "The length of the given string is "<<b;
+    return 0;
+}
